0bb2c53a67b71b3779ddeb7918e536c6537d7353
JIn <jin@JIndeMacBook-Air.local>
2025-10-07 16:16:39 +0800

    session create

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -18,14 +18,59 @@ def index(request):
     num_authors = Author.objects.count()
     num_genre =Genre.objects.count()
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
         'num_genre':num_genre ,
+        'num_visits': num_visits,
 
     }
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+    
+    # def get_queryset(self):
+    #     return Book.objects.filter(title__icontains='武俠')[:5] # Get 5 books 
+    
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(BookListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context['some_data'] = 'This is just some data'
+        return context
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    def book_detail_view(request, primary_key):
+        try:
+            book = Book.objects.get(pk=primary_key)
+        except Book.DoesNotExist:
+            raise Http404('Book does not exist')
+
+    # from django.shortcuts import get_object_or_404
+    # book = get_object_or_404(Book, pk=primary_key)
+        return render(request, 'catalog/book_detail.html', context={'book': book})
+
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    
+   
+
+class AuthorDetailView(generic.DetailView):
+    model = Author 
